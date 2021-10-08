2b6f1ec41384ff5c123434ed8f978006076b27ea
Ekaterina Kolesnikova <1005885@mail.ru>
2021-10-08 19:02:54 +0300

    HomeWork

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,16 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  let arr = [];
+  let d = (b ** 2) - (4 * a * c);
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    arr.push((-b) / 2 * a);
+  } else if (d > 0) {
+    arr.push(((-b) + Math.sqrt(d)) / (2 * a));
+    arr.push(((-b) - Math.sqrt(d)) / (2 * a));
+  }
+  return arr; 
 }
