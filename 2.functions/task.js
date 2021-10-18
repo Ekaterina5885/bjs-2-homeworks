4dfc9ada5e7e8526d86a2ba69fbc31c292f8d90f
Ekaterina Kolesnikova <1005885@mail.ru>
2021-10-18 09:51:46 +0300

    V1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,49 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  
+  let min = 101;
+  let max = -101;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min){
+      min = arr[i];
+    }
+    if (arr[i] > max){
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+  
+  avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
 
-  // Ваш код
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+   sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
 
-  // Ваш кода
-  // for ...
+  let max = 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+   if (func(arrOfArr[i]) > max) {
+     max = func(arrOfArr[i]);
+   }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  return Math.max(...arr) - Math.min(...arr);
 }
