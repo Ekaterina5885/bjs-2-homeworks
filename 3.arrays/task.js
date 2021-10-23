b0933b4c4ec8705e5aa31bf0c918c3380fc32500
Ekaterina Kolesnikova <1005885@mail.ru>
2021-10-23 10:48:26 +0300

    Home Work

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,21 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
-  return result; // boolean
+  if (arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index])) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result; 
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr
+     .filter((number) => number > 0)
+     .filter((number) => number % 3 === 0)
+     .map((number) => number * 10);
 
-  return resultArr; // array
+  return resultArr; 
 }
