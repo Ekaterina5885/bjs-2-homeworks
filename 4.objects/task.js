e03adc2cf833a4449dea50e50c125d7fa8e84b0c
Ekaterina Kolesnikova <1005885@mail.ru>
2021-11-01 07:48:59 +0300

    Update file

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -17,12 +17,16 @@ Student.prototype.addMark = function (mark) {
 }
 
 Student.prototype.addMarks = function (...marks) {  
-  this.marks = [...marks];
+  if (this.marks === undefined){ 
+    this.marks = marks; 
+  } else {
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {    
   let sum = 0;
-  for (i = 0; i < this.marks.length; i++) {
+  for (let i = 0; i < this.marks.length; i++) {
       sum += this.marks[i]; 
   }
 
